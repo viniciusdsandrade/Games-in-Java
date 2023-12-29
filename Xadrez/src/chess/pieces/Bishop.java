b2d1eb71e6714f662b7842ef68e94b77685275b2
Vinicius <vinicius_andrade2010@hotmail.com>
2023-12-29 14:35:08 -0300

    build: 200 - Movimentação 'Bispo'

--- Xadrez/src/chess/pieces/Bishop.java
+++ Xadrez/src/chess/pieces/Bishop.java
@@ -6,23 +6,56 @@ import chess.ChessPiece;
 import chess.Color;
 
 public class Bishop extends ChessPiece {
-    
+
     public Bishop(Board board, Color color) {
         super(board, color);
     }
-    
+
     @Override
     public String toString() {
         return "B";
     }
 
+    /**
+     * Gera uma matriz de booleanos indicando os possíveis movimentos do bispo no tabuleiro.
+     * O bispo pode mover-se em diagonais, tanto para cima quanto para baixo, desde que não haja
+     * peças bloqueando o caminho, e ele pode capturar peças adversárias.
+     *
+     * @return Matriz de booleanos representando os possíveis movimentos do bispo.
+     */
     @Override
     public boolean[][] possibleMoves() {
-        // TODO 
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
         Position p = new Position(0, 0);
 
+        // Movimentos diagonais
+        addDiagonalMoves(mat, p, -1, -1); // diagonal superior esquerda
+        addDiagonalMoves(mat, p, -1, 1);  // diagonal superior direita
+        addDiagonalMoves(mat, p, 1, -1);  // diagonal inferior esquerda
+        addDiagonalMoves(mat, p, 1, 1);   // diagonal inferior direita
+
         return mat;
+    }
+
+    /**
+     * Adiciona movimentos diagonais à matriz de movimentos possíveis.
+     *
+     * @param mat       A matriz de booleanos representando os possíveis movimentos.
+     * @param p         A posição a ser verificada.
+     * @param rowChange A mudança na coordenada da linha (positiva para baixo, negativa para cima).
+     * @param colChange A mudança na coordenada da coluna (positiva para a direita, negativa para a esquerda).
+     */
+    private void addDiagonalMoves(boolean[][] mat, Position p, int rowChange, int colChange) {
+        p.setValues(position.getRow() + rowChange, position.getColumn() + colChange);
+
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() + rowChange, p.getColumn() + colChange);
+        }
+
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
     }
 }
--- Xadrez/src/chess/pieces/King.java
+++ Xadrez/src/chess/pieces/King.java
@@ -16,27 +16,39 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    /**
+     * Verifica se a peça pode se mover para a posição especificada.
+     *
+     * @param position A posição a ser verificada.
+     * @return true se a peça pode se mover para a posição, false caso contrário.
+     */
     private boolean canMove(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p == null || p.getColor() != getColor();
     }
 
+    /**
+     * Gera uma matriz de booleanos indicando os possíveis movimentos do rei no tabuleiro.
+     * O rei pode mover-se para qualquer posição adjacente (acima, abaixo, esquerda, direita
+     * ou em qualquer diagonal) desde que não haja peças bloqueando o caminho.
+     *
+     * @return Matriz de booleanos representando os possíveis movimentos do rei.
+     */
     @Override
     public boolean[][] possibleMoves() {
-
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
         Position p = new Position(0, 0);
 
         int[][] directions = {
-                {-1, 0}, // acima
-                {1, 0}, // abaixo
-                {0, -1}, // esquerda
-                {0, 1}, // direita
-                {-1, -1}, // diagonal superior esquerda
-                {-1, 1}, // diagonal superior direita
-                {1, -1}, // diagonal inferior esquerda
-                {1, 1} // diagonal inferior direita
+                {-1, 0},    // acima
+                {1, 0},     // abaixo
+                {0, -1},    // esquerda
+                {0, 1},     // direita
+                {-1, -1},   // diagonal superior esquerda
+                {-1, 1},    // diagonal superior direita
+                {1, -1},    // diagonal inferior esquerda
+                {1, 1}      // diagonal inferior direita
         };
 
         for (int[] direction : directions) {
