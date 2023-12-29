bb636389e6f711c895f90bb763fe1dff8d822ac1
Vinicius <vinicius_andrade2010@hotmail.com>
2023-12-29 14:54:09 -0300

    build: 200 - 203 - Movimentação 'cavalo e rainha'

--- Xadrez/src/chess/pieces/Knight.java
+++ Xadrez/src/chess/pieces/Knight.java
@@ -16,13 +16,52 @@ public class Knight extends ChessPiece {
         return "N";
     }
 
+    /**
+     * Gera uma matriz de booleanos indicando os possíveis movimentos do cavalo no tabuleiro de xadrez.
+     * O cavalo pode realizar movimentos em "L", pulando sobre outras peças. Os movimentos são representados
+     * por deslocamentos relativos, permitindo movimentos em diversas direções.
+     *
+     * @return Matriz de booleanos representando os possíveis movimentos do cavalo.
+     */
     @Override
     public boolean[][] possibleMoves() {
-        // TODO 
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        // Movimentos em L
+        int[][] moves = {
+                {-1, -2}, {-2, -1}, // Movimento acima
+                {-2, 1}, {-1, 2},   // Movimento à esquerda
+                {1, 2}, {2, 1},     // Movimento abaixo
+                {2, -1}, {1, -2}    // Movimento à direita
+        };
 
         Position p = new Position(0, 0);
 
+        for (int[] move : moves) {
+            p.setValues(position.getRow() + move[0], position.getColumn() + move[1]);
+
+            // Verifica se a posição é válida e se o cavalo pode mover para lá
+            if (getBoard().positionExists(p) && canMove(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        }
+
         return mat;
     }
+
+    /**
+     * Verifica se o cavalo pode mover para a posição especificada.
+     * O cavalo pode mover para uma posição se ela estiver vazia (sem peça) ou contiver uma peça adversária.
+     *
+     * @param p A posição a ser verificada.
+     * @return true se o cavalo pode mover para a posição, false caso contrário.
+     */
+    private boolean canMove(Position p) {
+        // Obtém a peça na posição especificada no tabuleiro
+        ChessPiece piece = (ChessPiece) getBoard().piece(p);
+
+        // Retorna true se a posição estiver vazia ou se contiver uma peça adversária
+        return piece == null || piece.getColor() != getColor();
+    }
+
 }
--- Xadrez/src/chess/pieces/Queen.java
+++ Xadrez/src/chess/pieces/Queen.java
@@ -9,7 +9,7 @@ public class Queen extends ChessPiece {
     public Queen(Board board, Color color) {
         super(board, color);
     }
-    
+
     @Override
     public String toString() {
         return "Q";
@@ -17,11 +17,41 @@ public class Queen extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        // TODO 
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
         Position p = new Position(0, 0);
 
+        moveInDirection(mat, p, -1, 0);  // vertical acima
+        moveInDirection(mat, p, 1, 0);   // vertical abaixo
+        moveInDirection(mat, p, 0, -1);  // horizontal à esquerda
+        moveInDirection(mat, p, 0, 1);   // horizontal à direita
+        moveInDirection(mat, p, -1, -1); // diagonal superior esquerda
+        moveInDirection(mat, p, -1, 1);  // diagonal superior direita
+        moveInDirection(mat, p, 1, -1);  // diagonal inferior esquerda
+        moveInDirection(mat, p, 1, 1);   // diagonal inferior direita
+
         return mat;
     }
+
+    /**
+     * Move na direção especificada até encontrar uma peça ou alcançar os limites do tabuleiro.
+     *
+     * @param mat       A matriz de booleanos representando os possíveis movimentos.
+     * @param p         A posição inicial.
+     * @param rowChange A mudança na coordenada da linha.
+     * @param colChange A mudança na coordenada da coluna.
+     */
+    private void moveInDirection(boolean[][] mat, Position p, int rowChange, int colChange) {
+        p.setValues(position.getRow() + rowChange, position.getColumn() + colChange);
+
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() + rowChange, p.getColumn() + colChange);
+        }
+
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+    }
+
 }
